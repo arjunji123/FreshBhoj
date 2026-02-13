b80b86404e80a7a51443324608eb935bd8eb8b1e
nikhil tripathi <nt001230007@gmail.com>
2026-02-13 17:23:03 +0530

    build sucess

--- src/components/VendorForm.tsx
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-
-function VendorForm() {
-    const [name, setName] = useState();
-}
--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,6 @@ createRoot(document.getElementById('root')!).render(
     <Showcase/>
     <Vendor/>
   <Customer/>
+  
   </StrictMode>,
 )
